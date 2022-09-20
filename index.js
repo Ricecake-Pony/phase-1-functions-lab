5ea67db881db5ce5ad1d2976a3a77fa91857b915
Trey <trey.delcamp@gmail.com>
2022-09-20 16:59:36 -0500

    commit tree 331578dbe3f640f95ccae68e4540b8388331e509

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution in this file!
+//function distanceFromHqInBlocks(remainingBlocks) {
+ //   let streetLocation = remainingBlocks - 42;
+   // console.log(remainingBlocks);
+    //console.log(streetLocation, remainingBlocks);
+
+    //return Math.abs(streetLocation);  
+  //}
+  
+  function distanceFromHqInBlocks(pickUp) {
+    if (pickUp > 42){
+      return pickUp - 42;
+   } else {
+      return 42 - pickUp;
+    }
+  
+  }
+
+  function distanceFromHqInFeet(blockNumber) {
+    let remainingBlocks = distanceFromHqInBlocks(blockNumber);
+
+    return remainingBlocks * 264;
+    
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    //let distanceCovered = Math.abs(start - destination) *264;
+    if (start > destination){
+      return (start - destination) * 264;
+    } else {
+      return (destination - start) * 264;
+    }
+
+   console.log(distanceTravelledInFeet);
+
+   //return distanceCovered;
+  }
+
+  function calculatesFarePrice(startBlock, endBlock) {
+    //returns the fare for the customer
+
+      let distance = distanceTravelledInFeet(startBlock, endBlock);
+      console.log(distance)
+      
+     if (distance <= 400){
+        return 0;
+      } else if (distance <= 2000 ){
+         return (distance - 400) * .02;
+      } else if ( distance <= 2500){
+  return 25
+      } else { distance > 2500
+  
+     return "cannot travel that far";
+    }
+  }
